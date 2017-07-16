554a37b2aef61df144bba199b062956aa65b290f
Pui Lam CHEUNG <160039700@stu.vtc.edu.hk>
2017-07-16 17:58:55 +0800

    photo upload done
    -to do : add to db

--- php/dbInfo.php
+++ php/dbInfo.php
@@ -3,4 +3,6 @@
 		$username = "KC@wbmss";
 		$password = "gpa4.04.0";
 		$database = "projectDB";
+
+		$conn = mysqli_connect($hostname, $username, $password, $database);
 ?>
--- php/register.php
+++ php/register.php
@@ -47,6 +47,49 @@
     }
   }
 
+  $target_dir = "../upload/profile_pic/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+  // Check if image file is a actual image or fake image
+  if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) {
+          $uploadOk = 1;
+      } else {
+          $uploadOk = 0;
+      }
+  }
+  // Check if file already exists
+  if (file_exists($target_file)) {
+      showAlert("Sorry, file already exists.");
+      $uploadOk = 0;
+  }
+  // Check file size
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+      showAlert("Sorry, your photo is too large.");
+      $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+      showAlert("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+      $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+      showAlert("Sorry, your photo was not uploaded.");
+  // if everything is ok, try to upload file
+  } else {
+      $temp = explode(".", $_FILES["fileToUpload"]["tmp_name"]);
+      $newfilename = round(microtime(true));
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "../upload/profile_pic/" . $newfilename . "." . $imageFileType)) {
+          //showAlert("The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.");
+      } else {
+          showAlert("Sorry, there was an error uploading your photo.");
+      }
+  }
+
   if($empty) {
     showAlert("The following field must not be null or white space. $emptyField");
   } else if ($lengthError){
@@ -55,55 +98,16 @@
     showAlert("The email address you enter is not valid.");
   } else if (strcmp($input["Password"], $input["Comfirm Password"])) {
     showAlert("The password do not match.");
-  } else {
-
+  } else if ($uploadOk != 0){
+    
   }
 
-  if(!is_dir('../image/profile_pic')) {
-    mkdir('../image/profile_pic', 0755, true);
-	}
-	$target_dir = "../image/profile_pic";
-	$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = pathinfo($target_dir, PATHINFO_EXTENSION);
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if ($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-    }
 
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
+
   function showAlert($message) {
     echo "<script type='text/javascript'>alert('$message');</script>";
   }
+
   ?>
 </body>
 </html>
