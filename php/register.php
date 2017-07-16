e9197e6f301f095759694fe6d2bab0a139c35b60
Pui Lam CHEUNG <160039700@stu.vtc.edu.hk>
2017-07-17 04:14:53 +0800

    register UI + POST
    cannot do upload image function

--- php/register.php
+++ php/register.php
@@ -11,6 +11,7 @@
   $input["First Name"] = $fname;
   $input["Last Name"] = $lname;
   $input["Gender"] = $Gender;
+  $input["Date of Birth"] = $DOB;
   $input["Email Address"] = $email;
   $input["Country"] = $ctry;
   $input["Password"] = $pass1;
@@ -54,9 +55,52 @@
     showAlert("The email address you enter is not valid.");
   } else if (strcmp($input["Password"], $input["Comfirm Password"])) {
     showAlert("The password do not match.");
+  } else {
+
   }
 
+  if(!is_dir('../image/profile_pic')) {
+    mkdir('../image/profile_pic', 0755, true);
+	}
+	$target_dir = "../image/profile_pic";
+	$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = pathinfo($target_dir, PATHINFO_EXTENSION);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+      echo "File is an image - " . $check["mime"] . ".";
+      $uploadOk = 1;
+    } else {
+      echo "File is not an image.";
+      $uploadOk = 0;
+    }
 
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
   function showAlert($message) {
     echo "<script type='text/javascript'>alert('$message');</script>";
   }
