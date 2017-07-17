5e5a4808fcf4bb0e504aebac75e932a1aeae122b
Pui Lam CHEUNG <160039700@stu.vtc.edu.hk>
2017-07-18 01:58:28 +0800

    Login modified
    - changed to ID

--- php/login.php
+++ php/login.php
@@ -8,13 +8,26 @@
 <body>
   <?php
   extract($_POST);
-  $input["Email Address"] = $email;
+  $input["id"] = $id;
   $input["Password"] = $Password;
 
   require_once("../php/dbInfo.php");
 
   $empty = false;
   $emptyField = "";
+  $pkName = "";
+
+  switch ($type) {
+    case 'runner':
+      $pkName = "RunnerID";
+      break;
+    case 'volunteer':
+      $pkName = "VolunteerID";
+      break;
+    case 'sponsor':
+      $pkName = "SponsorID";
+      break;
+  }
   //check empty input
   foreach($input as $inputName => $value) {
     if (!isset($value) || trim($value)==='') {
@@ -25,31 +38,29 @@
 
   if($empty) {
     returnPage("The following field must not be null or white space. $emptyField");
-  } else if (!filter_var($input["Email Address"], FILTER_VALIDATE_EMAIL)) {
-    returnPage("The password do not match.");
   } else {
     if (!$conn) {
       die("Connection Fail". mysqli_connect_error());
     } else {
-      $query = "SELECT * FROM $type WHERE Email = '$email' AND Password = '12'";
+      $query = "SELECT * FROM $type WHERE $pkName = '$id' AND Password = '$Password'";
       $result = mysqli_query($conn, $query);
       $row = mysqli_fetch_array($result);
       if(mysqli_num_rows($result) == 1){
         $name = "$row[FirstName] $row[LastName]";
         $time = 3600*2;
-        setcookie("userName", $name, time()+$time);
-        setcookie("userType", $type, time()+$time);
+        setcookie("userName", $name, time()+$time, "/");
+        setcookie("userType", $type, time()+$time, "/");
         switch ($type) {
           case 'runner':
-            setcookie("userID",$row["RunnerID"], time()+$time);
+            setcookie("userID",$row["RunnerID"], time()+$time, "/");
             header("location: ../html/runnerindex.php");
             break;
           case 'volunteer':
-            setcookie("userID",$row["volunteerID"], time()+$time);
+            setcookie("userID",$row["volunteerID"], time()+$time, "/");
             header("location: ../html/volunteerindex.html");
             break;
           case 'sponsor':
-            setcookie("userID",$row["SponsorID"], time()+$time);
+            setcookie("userID",$row["SponsorID"], time()+$time, "/");
             header("location: ../html/sponsorindex.html");
             break;
           default:
